7a26fb82664355154ab5e7c1608bd48dcb248353
Adam Grzesik <adam.grzesik45@gmail.com>
2023-11-21 15:14:31 +0100

    Replace UserCreationForm with custom CreateRegistrationForm in registration views

--- registration/views.py
+++ registration/views.py
@@ -1,16 +1,16 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-
+from .forms import CreateRegistrationForm
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CreateRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('dashboard-index')
     else:
-        form = UserCreationForm()
+        form = CreateRegistrationForm()
     context = {
         'form': form,
     }
