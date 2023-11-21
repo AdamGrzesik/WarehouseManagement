dd0eb08289e2f9d44d33ad01cd725fffa8103ab5
Adam Grzesik <adam.grzesik45@gmail.com>
2023-11-21 14:56:34 +0100

    Add Order model to admin dashboard and refactor HTML templates

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Order
 from django.contrib.auth.models import Group
 
 admin.site.site_header = 'Warehouse Admin Dashboard'
@@ -12,4 +12,6 @@ class ProductAdmin(admin.ModelAdmin):
 
 # Register your models here.
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Order)
+
 admin.site.unregister(Group)
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,7 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-
-# Create your models here.
 CATEGORY = (
     ('Office', 'Office'),
     ('Electronics', 'Electronics'),
@@ -9,6 +8,7 @@ CATEGORY = (
 )
 
 
+# Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=100, null=True)
     category = models.CharField(max_length=20, choices=CATEGORY, null=True)
@@ -16,3 +16,13 @@ class Product(models.Model):
 
     def __str__(self):
         return f'{self.name}: {self.quantity}'
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    staff = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(null=True)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.product}, requested by {self.staff}'
