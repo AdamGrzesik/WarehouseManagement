5d5b0cfb8270805bcbaa892937a63abfc143d4f5
Adam Grzesik <adam.grzesik45@gmail.com>
2023-11-24 16:57:03 +0100

    Enhancing routing, rendering and templates

--- registration/views.py
+++ registration/views.py
@@ -1,6 +1,6 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from .forms import CreateRegistrationForm
+
 
 # Create your views here.
 def register(request):
@@ -8,10 +8,18 @@ def register(request):
         form = CreateRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('dashboard-index')
+            return redirect('login-user')
     else:
         form = CreateRegistrationForm()
     context = {
         'form': form,
     }
     return render(request, 'registration/register.html', context)
+
+
+def login(request):
+    return render(request, 'registration/login.html')
+
+
+def logout(request):
+    return render(request, 'registration/logout.html')
