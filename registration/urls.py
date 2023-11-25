2f7c2bd92fbe278689ef86d70822a7217a438e16
Adam Grzesik <adam.grzesik45@gmail.com>
2023-11-25 20:27:58 +0100

    Implemented user profile update functionality.

--- registration/urls.py
+++ registration/urls.py
@@ -7,6 +7,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('register/', views.register, name='register-user'),
     path('profile/', views.profile, name='user-profile'),
+    path('profile/update', views.profile_update, name='user-profile-update'),
     path('', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login-user'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout-user'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- registration/views.py
+++ registration/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import CreateRegistrationForm
+from .forms import CreateRegistrationForm, UpdateUserForm, UpdateProfileForm
 
 
 # Create your views here.
@@ -27,3 +27,21 @@ def logout(request):
 
 def profile(request):
     return render(request, 'registration/profile.html')
+
+
+def profile_update(request):
+    if request.method == 'POST':
+        form = UpdateUserForm(request.POST, instance=request.user)
+        profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if form.is_valid() and profile_form.is_valid():
+            form.save()
+            profile_form.save()
+            return redirect('user-profile')
+    else:
+        form = UpdateUserForm(instance=request.user)
+        profile_form = UpdateProfileForm(instance=request.user.profile)
+    context = {
+        'form': form,
+        'profile_form': profile_form,
+    }
+    return render(request, 'registration/profile_update.html', context)
