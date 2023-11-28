1f4aabab8a0cb388a39d64635c37cc2c59560e57
Adam Grzesik <adam.grzesik45@gmail.com>
2023-11-28 19:46:49 +0100

    Enhance user feedback and form handling in dashboard and registration views

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,8 +1,18 @@
 from django import forms
-from .models import Product
+from .models import Product, Order
 
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'category', 'quantity']
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['product', 'quantity']
+
+    def __init__(self, *args, **kwargs):
+        super(OrderForm, self).__init__(*args, **kwargs)
+        self.fields['product'].choices = [(product.id, product.name) for product in Product.objects.all()]
