d91eeac7fa7370d77ca1fc33a87043a2f05d396a
AdamGrzesik <115880425+AdamGrzesik@users.noreply.github.com>
2023-12-01 23:00:51 +0100

    Revert "Feature/migrating to PostgreSQL (#9) (#9)"
    This reverts commit 5ca85dd334f78079687c33903ddda9e8c12fa8b6.

--- warehouseProject/settings.py
+++ warehouseProject/settings.py
@@ -11,11 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url
-import environ
-
-env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,12 +19,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-#su@2+#9)^a-q=v@*^j+29o@ao4ude742f+_y-ze!0e--jo4=7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'warehouse-management-application.onrender.com',
+    '127.0.0.1',
+]
 
 # Application definition
 
@@ -82,19 +80,11 @@ WSGI_APPLICATION = 'warehouseProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
-'''
-
-# PostgreSQL database
-
-DATABASES = {
-    'default': dj_database_url.parse(env('DATABASE_URL'))
 }
 
 # Password validation
@@ -154,5 +144,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'warehouse.project.ag@gmail.com'
+EMAIL_HOST_PASSWORD = 'arxz vnud audt zkmp'
