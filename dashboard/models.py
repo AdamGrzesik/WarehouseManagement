e2dadf9bc41e9082b913f94dc387ce740118a7fa
Adam Grzesik <adam.grzesik45@gmail.com>
2023-12-01 16:40:37 +0100

    Extended product categories and added dynamic message display feature

--- dashboard/models.py
+++ dashboard/models.py
@@ -2,8 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 CATEGORY = (
-    ('Office', 'Office'),
+    ('Clothing', 'Clothing'),
     ('Electronics', 'Electronics'),
+    ('Food', 'Food'),
+    ('Health and Beauty', 'Health and Beauty'),
+    ('Office', 'Office'),
+    ('Sports and Outdoors', 'Sports and Outdoors'),
+    ('Tools', 'Tools'),
+    ('Toys and Games', 'Toys and Games'),
     ('Miscellaneous', 'Miscellaneous'),
 )
 
@@ -26,3 +32,27 @@ class Order(models.Model):
 
     def __str__(self):
         return f'{self.product}, requested by {self.staff}'
+
+
+class SingletonModel(models.Model):
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        self.pk = 1
+        super(SingletonModel, self).save(*args, **kwargs)
+
+    @classmethod
+    def load(cls):
+        obj, created = cls.objects.get_or_create(pk=1)
+        return obj
+
+
+class Message(SingletonModel):
+    text = models.CharField(max_length=300, default="Remember to complete your profile info!", null=False)
+
+    class Meta:
+        verbose_name_plural = 'Message'
+
+    def __str__(self):
+        return self.text
