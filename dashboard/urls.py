17a8fac9c10cf32ca64181c7981d11098925bf2b
Adam Grzesik <adam.grzesik45@gmail.com>
2023-12-03 19:42:50 +0100

    Added 'Export to CSV' functionality

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('dashboard/', views.index, name='dashboard-index'),
     path('staff/', views.staff, name='dashboard-staff'),
     path('staff/view/<int:primary_key>/', views.staff_view, name='dashboard-staff-view'),
-    path('product/', views.product, name='dashboard-product'),
-    path('product/delete/<int:primary_key>/', views.product_delete, name='dashboard-product-delete'),
-    path('product/edit/<int:primary_key>/', views.product_update, name='dashboard-product-update'),
-    path('order/', views.order, name='dashboard-order'),
+    path('products/', views.product, name='dashboard-product'),
+    path('products/delete/<int:primary_key>/', views.product_delete, name='dashboard-product-delete'),
+    path('products/edit/<int:primary_key>/', views.product_update, name='dashboard-product-update'),
+    path('orders/', views.order, name='dashboard-order'),
+    # CSV export
+    path('orders/csv', views.export_orders_to_csv, name='export-orders-to-csv'),
+    path('products/csv', views.export_products_to_csv, name='export-products-to-csv'),
 ]
