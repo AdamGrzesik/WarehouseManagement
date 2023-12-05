b714d03ecfdd2ab9789d87a0df94eeb9e292c4ca
Adam Grzesik <adam.grzesik45@gmail.com>
2023-12-05 14:13:35 +0100

    Removing unused import

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Product, Order, Message
 from .forms import ProductForm, OrderForm, OrderUpdateForm
