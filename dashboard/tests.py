ee0e8ac0d9deb9046ec556dc11a095c754d6da10
Adam Grzesik <adam.grzesik45@gmail.com>
2023-12-06 18:15:55 +0100

    Example tests

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,52 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class DashboardIndexViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.dashboard_index_url = reverse('dashboard-index')
+
+    def test_dashboard_index_view_GET(self):
+        response = self.client.get(self.dashboard_index_url)
+        self.assertEqual(response.status_code, 302)
+
+
+class DashboardStaffViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.dashboard_staff_url = reverse('dashboard-staff')
+
+    def test_dashboard_staff_view_GET(self):
+        response = self.client.get(self.dashboard_staff_url)
+        self.assertEqual(response.status_code, 302)
+
+
+class DashboardProductViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.dashboard_product_url = reverse('dashboard-product')
+
+    def test_dashboard_product_view_GET(self):
+        response = self.client.get(self.dashboard_product_url)
+        self.assertEqual(response.status_code, 302)
+
+
+class DashboardOrderViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.dashboard_order_url = reverse('dashboard-order')
+
+    def test_dashboard_order_view_GET(self):
+        response = self.client.get(self.dashboard_order_url)
+        self.assertEqual(response.status_code, 302)
+
+
+class DashboardOrderUpdateViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.dashboard_order_update_url = reverse('dashboard-order-update', args=[1])
+
+    def test_dashboard_order_update_view_GET(self):
+        response = self.client.get(self.dashboard_order_update_url)
+        self.assertEqual(response.status_code, 302)
--- registration/tests.py
+++ registration/tests.py
@@ -1,3 +1,78 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class RegisterViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.register_url = reverse('register-user')
+
+    def test_register_view_GET(self):
+        response = self.client.get(self.register_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/register.html')
+
+    def test_register_view_POST(self):
+        response = self.client.post(self.register_url, {
+            'username': 'testuser',
+            'password1': 'password123',
+            'password2': 'password123',
+        })
+        self.assertEqual(response.status_code, 200)
+
+
+class LoginViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.login_url = reverse('login-user')
+
+    def test_login_view_GET(self):
+        response = self.client.get(self.login_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/login.html')
+
+
+class LogoutViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.logout_url = reverse('logout-user')
+
+    def test_logout_view_GET(self):
+        response = self.client.get(self.logout_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/logout.html')
+
+
+class ProfileViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.profile_url = reverse('user-profile')
+        self.user = User.objects.create_user(username='testuser', password='password123')
+
+    def test_profile_view_GET(self):
+        self.client.login(username='testuser', password='password123')
+        response = self.client.get(self.profile_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/profile.html')
+
+
+class ProfileUpdateViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.profile_update_url = reverse('user-profile-update')
+        self.user = User.objects.create_user(username='testuser', password='password123')
+
+    def test_profile_update_view_GET(self):
+        self.client.login(username='testuser', password='password123')
+        response = self.client.get(self.profile_update_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/profile_update.html')
+
+    def test_profile_update_view_POST(self):
+        self.client.login(username='testuser', password='password123')
+        response = self.client.post(self.profile_update_url, {
+            'username': 'updateduser',
+            'email': 'updateduser@example.com',
+        })
+        self.assertEqual(response.status_code, 200)
